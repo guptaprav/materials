0aa11fed7d295bad7288d3eb4fcb279e0a4180b4
Praveen Gupta <pgupta@Praveens-MacBook-Pro.local>
2020-08-08 15:23:14 -0700

    materials, tabs and button

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
 
 import logo from '../../assets/logo.svg';
 
@@ -22,25 +25,58 @@ function ElevationScroll(props) {
 const useStyles = makeStyles(theme => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
-    // marginBottom: '3em'
+    marginBottom: '3em'
   },
   logo: {
     height: '7em'
+  },
+  tabContainer: {
+    marginLeft: 'auto'
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '24px'
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight: '25px',
+    height: '45px'
   }
 }));
 
 export default function Header(props) {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+  const handleChange = (e, value) => {
+    setValue(value);
+  }
 
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position="fixed" color="primary">
-            <Toolbar>
-              <Typography variant="h3">
+            <Toolbar disableGutters>
+              {/* <Typography variant="h3">
                 Materials
-              </Typography>
-              {/* <img src={logo} className={classes.logo} alt="page logo" /> */}
+              </Typography> */}
+              <img src={logo} className={classes.logo} alt="page logo" />
+              <Tabs
+                className={classes.tabContainer}
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary">
+                <Tab className={classes.tab} label="Home" />
+                <Tab className={classes.tab} label="Product" />
+                <Tab className={classes.tab} label="Team" />
+                <Tab className={classes.tab} label="About Us" />
+                <Tab className={classes.tab} label="Contact Us" />
+              </Tabs>
+              <Button variant="contained" color="secondary" className={classes.button}>
+                Free Estimate
+              </Button>
             </Toolbar>
         </AppBar>
       </ElevationScroll>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -18,6 +18,18 @@ export default createMuiTheme({
   typography: {
     h3: {
       fontWeight: 300
+    },
+    tab: {
+      fontFamily: 'Raleway',
+      textTransform: 'none',
+      fontWeight: 700,
+      fontSize: '1rem'
+    },
+    estimate: {
+    fontFamily: 'Pacifico',
+    fontSize: '1rem',
+    textTransform: 'none',
+    color: 'white'
     }
   }
 });
